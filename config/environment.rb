012f7efde77b9dcef10d8de4f56f5e43f9f8bf9f
syahmiibrahim <syahmiibrahim@live.com>
2015-09-09 09:45:17 +0800

    first commit

---
+++ config/environment.rb
@@ -0,0 +1,50 @@
+# Set up gems listed in the Gemfile.
+# See: http://gembundler.com/bundler_setup.html
+#      http://stackoverflow.com/questions/7243486/why-do-you-need-require-bundler-setup
+ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
+
+require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
+
+# Require gems we care about
+require 'rubygems'
+
+require 'uri'
+require 'pathname'
+
+require 'pg'
+require 'active_record'
+require 'logger'
+
+require 'sinatra'
+require "sinatra/reloader" if development?
+
+require 'erb'
+
+require 'twitter'
+
+require 'byebug'
+
+require 'yaml'
+
+# Some helper constants for path-centric logic
+APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
+
+APP_NAME = APP_ROOT.basename.to_s
+
+# Set up the controllers and helpers
+Dir[APP_ROOT.join('app', 'controllers', '*.rb')].each { |file| require file }
+Dir[APP_ROOT.join('app', 'helpers', '*.rb')].each { |file| require file }
+
+# Set up the database and models
+require APP_ROOT.join('config', 'database')
+
+# Set up the Twitter API
+
+API_KEY = YAML::load_file(File.join(__dir__,'secret.yaml'))
+
+$twitter = Twitter::REST::Client.new do |config|
+  config.consumer_key = API_KEY["TWITTER_CONSUMER_KEY"]
+  config.consumer_secret = API_KEY["TWITTER_CONSUMER_SECRET"]
+  config.access_token = API_KEY["TWITTER_ACCESS_TOKEN"]
+  config.access_token_secret = API_KEY["TWITTER_ACCESS_TOKEN_SECRET"]
+end
