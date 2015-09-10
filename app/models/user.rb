152831a605e5233d70880b7461f20d023c30f2d8
syahmiibrahim <syahmiibrahim@live.com>
2015-09-10 09:37:43 +0800

    fix heroku deploy

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
       config.consumer_key = ENV['TWITTER_KEY']
       config.consumer_secret = ENV['TWITTER_SECRET']
       config.access_token = self.token #ENV['Access_Token']
-      config.access_token_secret = self.token #ENV['Access_Token_Secret']
+      config.access_token_secret = self.token_secret #ENV['Access_Token_Secret']
     end
      $twitter
   end
